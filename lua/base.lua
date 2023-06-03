d24829fd65f9ca1db6e3af8f8a97595d97b65727
tbm5k <tedburg5@gmail.com>
2023-06-03 13:44:19 +0300

    🎁 packages

--- lua/base.lua
+++ lua/base.lua
@@ -5,6 +5,7 @@ vim.opt.nu = true --:set number
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = 4
+vim.opt.expandtab = true
 
 vim.opt.smartindent = true
+vim.opt.termguicolors = true
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,25 +13,34 @@ packer.startup(function(use)
 	use({ 'rose-pine/neovim', as = 'rose-pine' })
 	vim.cmd('colorscheme rose-pine')
 
+    use 'wakatime/vim-wakatime'
+
 	use {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v2.x',
-  requires = {
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},             -- Required
-    {                                      -- Optional
-      'williamboman/mason.nvim',
-      run = function()
-        pcall(vim.cmd, 'MasonUpdate')
-      end,
-    },
-    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v2.x',
+      requires = {
+        -- LSP Support
+        {'neovim/nvim-lspconfig'},             -- Required
+        {                                      -- Optional
+          'williamboman/mason.nvim',
+          run = function()
+            pcall(vim.cmd, 'MasonUpdate')
+          end,
+        },
+        {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},     -- Required
-    {'hrsh7th/cmp-nvim-lsp'}, -- Required
-    {'L3MON4D3/LuaSnip'},     -- Required
-  }
-}
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},     -- Required
+        {'hrsh7th/cmp-nvim-lsp'}, -- Required
+        {'L3MON4D3/LuaSnip'},     -- Required
+      }
+    }
 
+    use {
+      'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+      requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 end)
