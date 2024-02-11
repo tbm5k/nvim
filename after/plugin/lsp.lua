d4ce1ae398995cf444cfd563508bdd679728adea
tbm5k <tedburg5@gmail.com>
2024-02-11 13:15:23 +0300

    pivot to mason to allow automated lsp server management

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,18 +1,36 @@
 local status, lsp = pcall(require, 'lsp-zero')
+local masonStatus, mason = pcall(require, 'mason')
+local masonLspStatus, masonLsp = pcall(require, 'mason-lspconfig')
 
 if (not status) then
 	print("Lsp zero not installed!")
 	return
 end
+if (not masonStatus) then
+	print("mason not installed!")
+	return
+end
+if (not masonLspStatus) then
+	print("mason-lspconfig not installed!")
+	return
+end
 
-lsp.setup_servers({
-	'tsserver',
-	'docker_compose_language_service',
-	'dockerls'
-})
+-- lsp.setup_servers({
+-- 	'tsserver',
+-- 	'docker_compose_language_service',
+-- 	'dockerls',
+-- })
 
 lsp.on_attach(function(client, bufnr)
 	lsp.default_keymaps({buffer = bufnr})
 end)
 
-lsp.setup()
+mason.setup()
+
+masonLsp.setup({
+  ensure_installed = {},
+  handlers = {
+    lsp.default_setup,
+  },
+})
+
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -100,6 +100,16 @@ _G.packer_plugins = {
     path = "/home/tbm5k/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
     url = "https://github.com/VonHeikemen/lsp-zero.nvim"
   },
+  ["mason-lspconfig.nvim"] = {
+    loaded = true,
+    path = "/home/tbm5k/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    url = "https://github.com/williamboman/mason-lspconfig.nvim"
+  },
+  ["mason.nvim"] = {
+    loaded = true,
+    path = "/home/tbm5k/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    url = "https://github.com/williamboman/mason.nvim"
+  },
   ["nvim-cmp"] = {
     loaded = true,
     path = "/home/tbm5k/.local/share/nvim/site/pack/packer/start/nvim-cmp",
